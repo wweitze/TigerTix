fcff81a57dcb61475fe3b2a997e73c1578675482
clemen8 <clemen8@clemson.edu>
2022-03-17 12:19:09 -0400

    Completed UserRepository class

--- TigerTix/Data/TigerTixContext.cs
+++ TigerTix/Data/TigerTixContext.cs
@@ -12,4 +12,17 @@ namespace TigerTix.Data
     {
         public DbSet<User> Users { get; set; }
     }
+
+    private readonly IConfiguration _config;
+
+    public TigerTixContext (IConfiguration config)
+    {
+        _config = config;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer(_config["ConnectionStrings:DefaultConnection"])''
+    }
 }
---
+++ TigerTix/Data/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+
+public class UserRepository
+{
+	private readonly TigerTixContext _context;
+
+	public UserRepository( TigerTixContext context)
+	{
+		_context = context;
+	}
+
+	//Save a user
+	public void SaveUser (User user)
+    {
+		_context.Add(user);
+		_context.SaveChanges();
+    }
+
+	//Returns all users
+	public IEnumerable<UserRepository> GetAllUsers()
+    {
+		var users = from u in _context.Users
+					select u;
+		return users.ToList();
+    }
+
+	public User GetUsersByID(int userID)
+    {
+		var user = (from u in _context.Users
+					where u.Id == userID
+					select u).FirstOrDefault();
+		return user;
+    }
+
+	//Update a user
+	public void UpdateUser(User user)
+    {
+		_context.Update(user);
+		_context.SaveChanges();
+    }
+
+	//Delete a user
+	public void DeleteUser(User user)
+    {
+		_context.Remove(user);
+		_context.SaveChanges();
+    }
+}
