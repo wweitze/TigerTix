62460377c8b4af311d3f5fd30d83e2a608602522
Vetas-Luna <mrippy@g.clemson.edu>
2022-05-01 23:04:43 -0400

    Added IsCustomer for the customer page

--- Data/Entities/User.cs
+++ Data/Entities/User.cs
@@ -9,5 +9,6 @@
         public string LastName { get; set; }
         public bool IsAdmin { get; set; }
         public bool IsCoordinator { get; set; }
+        public bool IsCustomer { get; set; }
     }
 }
